1040e9264860fc597254fc8d044d1ac4ddbd2d45
John Cipponeri <john.cipponeri@gmail.com>
2013-12-13 02:06:12 -0600

    - Added omnibox search

--- src/background.js
+++ src/background.js
@@ -1,7 +1,19 @@
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+function openWolframTab(text) {
 	chrome.tabs.create({
-		url: "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(info.selectionText)
+		url: "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(text)
 	});
+}
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest){
+	chrome.omnibox.setDefaultSuggestion({"description" : "Analyze with Wolfram: " + text});
+});
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+	openWolframTab(text);
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	openWolframTab(info.selectionText);
 });
 
 chrome.contextMenus.create({
